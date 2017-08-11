b5e39730f35d39a6721860809bf95d6570db806b
Jonny Pickard <jonathandpickard@gmail.com>
2017-08-11 15:00:30 +0100

    Refactored container imports

---
+++ src/containers/index.js
@@ -0,0 +1,3 @@
+export { default as PostsIndex } from './posts_index';
+export { default as PostsNew } from './posts_new';
+export { default as PostsShow } from './posts_show';
--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
-import PostsIndex from './containers/posts_index';
-import PostsNew from './containers/posts_new';
+import { PostsIndex, PostsNew, PostsShow } from './containers';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -28,6 +27,7 @@ ReactDOM.render(
         <div>
           <Switch>
             <Route path="/posts/new" component={PostsNew} />
+            <Route path="/posts/:id" component={PostsShow} />
             <Route path="/" component={PostsIndex} />
           </Switch>
         </div>
